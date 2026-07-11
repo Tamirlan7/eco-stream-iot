4be9e9de720127fce1d99a7036dc5a37ee66aaf8
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2026-07-11 21:08:11 +0500

    usage service

---
+++ usage-servcie/src/main/java/com/tami/usageservcie/UsageServcieApplication.java
@@ -0,0 +1,13 @@
+package com.tami.usageservcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageServcieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsageServcieApplication.class, args);
+    }
+
+}
---
+++ usage-servcie/src/test/java/com/tami/usageservcie/UsageServcieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tami.usageservcie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageServcieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
