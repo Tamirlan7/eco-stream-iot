3cdb8a37617f149e6dab7f50cbb955d12628eaa1
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2026-07-13 23:03:53 +0500

    usage service minor changes

---
+++ usage-servcie/src/main/java/com/tami/usageservice/dto/DeviceDto.java
@@ -0,0 +1,11 @@
+package com.tami.usageservice.dto;
+
+import lombok.Builder;
+
+@Builder
+public record DeviceDto(Long id,
+                        String name,
+                        String type,
+                        String location,
+                        Long userId) {
+}
