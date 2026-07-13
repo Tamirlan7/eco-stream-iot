f0df9debf86c059bb39877d5c4d622fb5c24fc38
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2026-07-14 04:32:58 +0500

    insight-service init

---
+++ insight-service/src/test/java/com/tami/insightservice/InsightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tami.insightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
